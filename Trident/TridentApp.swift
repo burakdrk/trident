4e8573d57b8eeb8ae6824cf414c12d8aa7a72013
Burak <bduruk@proton.me>
2025-07-23 02:09:33 -0400

    Initial Commit

---
+++ Trident/TridentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TridentApp.swift
+//  Trident
+//
+//  Created by Burak Duruk on 2025-07-23.
+//
+
+import SwiftUI
+
+@main
+struct TridentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TridentTests/TridentTests.swift
@@ -0,0 +1,17 @@
+//
+//  TridentTests.swift
+//  TridentTests
+//
+//  Created by Burak Duruk on 2025-07-23.
+//
+
+import Testing
+@testable import Trident
+
+struct TridentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
